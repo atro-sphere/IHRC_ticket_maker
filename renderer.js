dc3b485bcc214d0c0b4a1040cff0ec9030276b40
atro_sub <atro.quatro@gmail.com>
2026-06-06 17:37:13 +0900

    26/6/5/unknown

--- renderer.js
+++ renderer.js
@@ -12,13 +12,78 @@ async function save_img() {
     })
 }
 
+
+const ticket = document.getElementById("ticket_container")
+
+const operation_date_container = document.getElementById("operation_date")
+
+const operation_year_container = operation_date_container.querySelector("span#year")
+const operation_year = operation_year_container.querySelector("span.year")
+
+
 const selectors = document.getElementsByClassName("selector")
 
-for (const selector in selectors){
-    const options = selector.getElementsByClassName("option")
+for (const selector of selectors){
+    const pulldown = selector.querySelector(".pulldown") ?? null
 
-    selector.addEventListener("click", ()=>{
-        
+    if (pulldown){
+        const selected_el = selector.querySelectorAll(".selected_element")[0]
+
+        const selected_op = pulldown.querySelectorAll(".selected")[0]
+
+        selected_el.textContent = selected_op.textContent
+
+        console.log(pulldown)
+        selected_el.addEventListener("click", ()=>{
+            pulldown.style.display = "block"
+        })
+
+        const options = pulldown.querySelectorAll(".option")
+
+        for (const option of options){
+            option.addEventListener("click", ()=>{
+                selected_op.classList.remove("selected")
+
+                option.classList.add("selected")
+
+                selected_el.textContent = option.textContent
+
+                pulldown.style.display = "none"
+            })
+        }
+    }  
+}
+
+const inputs = document.getElementsByClassName("input")
+
+for (const input of inputs){
+    const selected_el = input.querySelectorAll(".selected_element")[0]
+
+    const input_el = input.querySelector("input")
+
+    selected_el.addEventListener("click", ()=>{
+        selected_el.style.display = "none"
+        input_el.style.display = "block"
+    })
+
+    input_el.addEventListener("keydown", (event)=>{
+        if (event.key === "Enter"){
+            const value = Number(input_el.value)
+
+            if (isNaN(value)){
+                console.error(`Invalid input: ${input_el.value}`)
+            }
+            else{
+                if (1954 > value){
+                    console.log(`Value ${value} is before the foundation of JRA.`)
+                }
+                selected_el.textContent = value
+                operation_year.textContent = value
+                selected_el.style.display = "block"
+
+                input_el.style.display = "none"
+            }
+        }
     })
 }
 
